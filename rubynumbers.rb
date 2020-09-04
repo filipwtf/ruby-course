bae6ea64362e53c94c30698fbc03acd37a855451
filipwtf <filipwedzicha@gmail.com>
2020-09-05 00:47:27 +0100

    ruby numbers, operators

---
+++ rubynumbers.rb
@@ -0,0 +1,23 @@
+=begin
+    Multiline comment
+    Ruby Numbers 
+    Usual operators: +, -, *, /
+=end
+
+puts 1 + 1
+puts 2 * 3
+
+# Integer division
+# When you do arithmetic with integers, you'll get an integer answer
+puts 3 / 2
+puts 10 -11
+puts 1.5 / 2.6
+puts "\n"
+
+puts 2**2
+
+puts "Modulo"
+puts (5 % 3)     # prints  2  
+puts (-5 % 3)    # prints  1  
+puts (5 % -3)    # prints -1  
+puts (-5 % -3)   # prints -2  
---
+++ rubyoperators.rb
@@ -0,0 +1,10 @@
+puts "Operators"
+puts nil || 2008
+puts false || 2008
+puts "ruby" || 2008
+
+puts "\n"
+# Assinging a variable if that variable isn't set
+@variable ||= "default value"  
+puts @variable
+
