3ca97ced3f910c6a58f59c324281a634d2b754ea
filipwtf <filipwedzicha@gmail.com>
2020-09-05 01:24:45 +0100

    Scopes

---
+++ rubyscopes.rb
@@ -0,0 +1,15 @@
+# Global variables start with a $ (dollar-sign)
+# Ruby starts with a large number of vraibles already initialized
+
+# File name
+puts $0
+
+# Ruby directories
+puts $:
+
+# Process id
+puts $$
+
+# Top level has its own scope
+# class or module (class, module) block has its own local socpes
+# method (def) has its own local scope
